d7ef92f19a7de547b09c2980f7c4e9dae2a268f2
noah-zll <2510472895@qq.com>
2026-01-18 20:24:32 +0800

    增加RuntimeAssemblyNames程序集

--- Scripts/Editor/Misc/Type.cs
+++ Scripts/Editor/Misc/Type.cs
@@ -22,6 +22,7 @@ namespace UnityGameFramework.Editor
             "UnityGameFramework.Runtime",
 #endif
             "Assembly-CSharp",
+            "UGF.GameFramework.Data",
         };
 
         private static readonly string[] RuntimeOrEditorAssemblyNames =
